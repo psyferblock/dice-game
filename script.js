cec6edc50628a9bee2a97dcb6468dc9e146a00af
psyfer <psyferblock.io@gmail.com>
2022-05-23 11:22:50 +0300

    cleaning the code

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
+// the array of images
+
 var images =["images/dice1.png","images/dice2.png","images/dice3.png","images/dice4.png","images/dice5.png","images/dice6.png"];
+
+// the 2 image tags to use for the pics 
 
 var img1 = document.querySelectorAll(".img1");
 var img2 = document.querySelectorAll(".img2");
 
+// the status of the game 
+
 var winner = document.getElementById("status");
 
 
+// the random numbers to compare the winner 
 
 let random1= randomFunc(0,images.length);
 let random2= randomFunc(0,images.length);
 
 
+// changing the source of the immages 
 
 img1[0].src= images[random1];
 img2[0].src= images[random2];
 
 
-
+// the if statements that change the html 
 
     if (random1>random2){
         winner.innerHTML = `winner is player1`
@@ -26,7 +34,7 @@ img2[0].src= images[random2];
     }
 
     
-
+// the function that randomises everything 
 
 function randomFunc(min,max){
    return Math.floor(Math.random()*(max-min)+min);
